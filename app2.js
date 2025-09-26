5abdd9fd17ce91cb9357b5f70830965a6dc18d44
WinnieW685 <wwu82648@gmail.com>
2025-09-26 09:34:55 -0700

    qwetrqwer

--- app2.js
+++ app2.js
@@ -23,11 +23,39 @@ if (!citizen) {
  && = and
 */
 
-function enter(hasKey, hasMagic, isBanned) {
+/* function enter(hasKey, hasMagic, isBanned) {
   if ((hasKey == true || hasMagic == true) && !isBanned) {
     console.log("you're princess is in another castle");
   } else {
     console.log("cannot enter");
   }
 }
-enter(true, true, true);
+enter(true, true, true); */
+
+/*function enter(rideHeightOK, withAdult, healthHold) {
+  if ((rideHeightOK == true || withAdult == true) && !healthHold) {
+    console.log("You're allowed to ride the rollercoaster");
+  } else {
+    console.log("cannot ride");
+  }
+}
+enter(true, true, false); */
+
+/*function enter(doorOpen, windowOpen) {
+  const alarmSilent = !(doorOpen || windowOpen);
+}
+// Equivalent by De Morgan: (!doorOpen && !windowOpen)
+enter(true, false);*/
+
+function middle(x, y, z) {
+  if ((x >= y && x <= z) || (x <= y && x >= z)) {
+    console.log("x is middle");
+  } else if ((y >= x && y <= z) || (y <= x && y >= z)){
+    console.log("y the middle");
+  } else ((z >= x && z <= y) || (z <= x && z >= y)) {
+    console.log("z is the middle")
+  }
+}
+
+middle(10, 20, ); */
+
