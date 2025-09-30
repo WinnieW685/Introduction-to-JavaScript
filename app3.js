7c1f2604dd6cd9ba6f62e639d521ac693d24ca5f
WinnieW685 <wwu82648@gmail.com>
2025-09-30 09:34:21 -0700

    mhgfhgfhgf

---
+++ app3.js
@@ -0,0 +1,21 @@
+/* const sentence =
+  "The red cat sat on the mat. Why are you so sad cat? Don't ask that.";
+
+function lang(sentence) {
+  let t = 0;
+  let s = 0;
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === "S" || sentence[1] === "s") {
+      s = s + 1;
+    } else if (sentence[i] === "T" || sentence[1] === "t") {
+      t++;
+    }
+  }
+  if (t > s) {
+    console.log("English");
+  } else {
+    console.log("Parle Vu Francaise");
+  }
+}
+lang(sentence);
+ */
